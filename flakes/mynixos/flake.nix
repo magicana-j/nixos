da83460dbc597ea34ccefa4c39ab6e2b4a3c7113
magicana-j <zima0524@hotmail.co.jp>
2024-06-01 18:31:05 +0900

    make

---
+++ flakes/mynixos/flake.nix
@@ -0,0 +1,39 @@
+{
+  description = "A very basic flake";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-23.11";
+    home-manager = {
+      url = "github:nix-community/home-manager";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+  };
+
+  outputs = { self, nixpkgs, ... }@inputs: {
+    nixosConfigurations.mynixos = nixpkgs.lib.nixosSystem {
+      system = "x86_64-linux";
+      modules = [
+        ./configuration.nix
+      ];
+    };
+
+    homeConfigurations = {
+      myHome = inputs.home-manager.lib.homeManagerConfiguration {
+        pkgs = import inputs.nixpkgs {
+          system = "x86_64-linux";
+          config.allowUnfree = true;
+        };
+        extraSpecialArgs = {
+          inherit inputs;
+        };
+        modules = [
+          ./home.nix
+        ];
+      };
+    };
+
+    #packages.x86_64-linux.hello = nixpkgs.legacyPackages.x86_64-linux.hello;
+    #packages.x86_64-linux.default = self.packages.x86_64-linux.hello;
+
+  };
+}
---
+++ flakes/mynixos/home.nix
@@ -0,0 +1,53 @@
+{config, pkgs, ... }:
+{
+  home = rec {
+    username = "junin";
+    homeDirectory = "/home/${username}";
+    stateVersion = "23.11";
+  };
+  programs.home-manager.enable = true;
+
+  home.packages = with pkgs; [
+    gcc go nodejs-slim nodePackages.wrangler
+    python312
+    discord
+    discord-ptb
+    gnome.totem
+    gnome.evince
+    parsec-bin
+    remmina
+    slack
+    vscode
+    easyeffects
+  ];
+
+  programs.git = {
+    enable = true;
+    userName = "magicana-j";
+    userEmail = "zima0524@hotmail.co.jp";
+  };
+
+  programs.gh = {
+    enable = true;
+    extensions = with pkgs; [gh-markdown-preview];
+    settings = {
+      editor = "nano";
+    };
+  };
+
+  programs.neovim = {
+    enable = true;
+
+    #extraLuaConfig = builtins.readFile ./init.lua;
+  };
+
+  programs.direnv = {
+    enable = true;
+    nix-direnv.enable = true;
+  };
+
+  programs.starship = {
+    enable = true;
+  };
+
+}
