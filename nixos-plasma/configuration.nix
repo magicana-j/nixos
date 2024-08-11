6ebe6dc1822112daa6511cbeeb578c90dc6bdb34
ruad_rofhessa <104673552+magicana-j@users.noreply.github.com>
2024-08-11 09:26:11 +0900

    Add files via upload

--- nixos-plasma/configuration.nix
+++ nixos-plasma/configuration.nix
@@ -8,7 +8,6 @@
   imports =
     [ # Include the results of the hardware scan.
       ./hardware-configuration.nix
-      ./users.nix
     ];
 
   # Bootloader.
@@ -26,6 +25,19 @@
   # Enable networking
   networking.networkmanager.enable = true;
 
+  # Open ports in the firewall.
+  # networking.firewall.allowedTCPPorts = [ ... ];
+  # networking.firewall.allowedUDPPorts = [ ... ];
+  # Or disable the firewall altogether.
+  # networking.firewall.enable = false;
+
+  services.tailscale.enable = true;
+  networking.firewall = {
+    enable = true;
+    trustedInterfaces = ["tailscale0"];
+    allowedUDPPorts = [];
+  };
+  
   # Set your time zone.
   time.timeZone = "Asia/Tokyo";
 
@@ -44,6 +56,33 @@
     LC_TIME = "ja_JP.UTF-8";
   };
 
+
+  # ENV
+  environment.variables = {
+    "WLR_NO_HARDWARE_CURSORS" = "1";
+    "WLR_RENDERER" = "pixman";
+    "WLR_RENDERER_ALLOW_SOFTWARE" = "1";
+    "LIBGL_ALWAYS_SOFTWARE" = "1";
+  };
+
+  # picom
+  services.picom.enable = true;
+  services.picom.fade = true;
+  services.picom.shadow = false;
+  services.picom.shadowExclude = [
+    "window_type *= 'toolbar'"
+    "window_type *= 'dock'"
+    "window_type *= 'desktop'"
+  ];
+  #services.picom.fadeExclude = [ 
+  #  "window_type *= 'menu'"
+  #];
+  #services.picom.fadeSteps = [
+  #  0.04
+  #  0.04
+  #];  
+
+
   # Enable the X11 windowing system.
   # You can disable this if you're only using the Wayland session.
   services.xserver.enable = true;
@@ -54,6 +93,69 @@
     wayland.enable = true;
   };
   services.desktopManager.plasma6.enable = true;
+
+  # lightdm Login Manager.
+#  services.xserver.displayManager.lightdm.enable = true;
+
+  # window manager
+  # swaywm
+    #programs.sway.enable = true;  
+    #programs.sway.extraPackages = with pkgs; [
+    #  waybar rofi slurp grim wf-recorder
+    #  fuzzel foot
+    #];
+
+  # hypr
+  #services.xserver.windowManager.hypr.enable = true;
+
+  # hyprland
+  #programs.hyprland.enable = true;
+  #programs.hyprland.enableNvidiaPatches = true;
+
+  # openbox
+  #services.xserver.windowManager.openbox.enable = true;
+
+  # Desktop Environtment
+  #services.xserver.desktopManager.lxqt.enable = true;
+  #services.xserver.desktopManager.budgie.enable = true;
+  #services.xserver.desktopManager.deepin.enable = true;
+  #services.xserver.desktopManager.xfce.enable = true;
+  #services.xserver.desktopManager.plasma5.enable = true;
+  #services.xserver.desktopManager.pantheon.enable = true;
+  #services.xserver.desktopManager.mate.enable = true;
+  #services.xserver.desktopManager.cinnamon.enable = true;
+  #services.xserver.desktopManager.gnome.enable = true;
+  #services.xserver.desktopManager.enlightenment.enable = true;
+
+  # gnome exclude
+  services.gnome.evolution-data-server.enable = pkgs.lib.mkForce false;
+  services.gnome.gnome-online-accounts.enable = pkgs.lib.mkForce false;
+  programs.gnome-terminal.enable = pkgs.lib.mkForce false;
+  environment.gnome.excludePackages = with pkgs; [
+    gnome.gnome-terminal
+    gnome.gnome-system-monitor
+    gnome.gnome-screenshot
+    gnome.gnome-music
+    gnome.gnome-keyring
+    gnome.file-roller
+    gnome.eog
+    gnome.yelp
+    gnome.totem
+    gnome.gedit
+    gnome.geary
+    gnome.cheese
+    orca
+    epiphany
+    gnome-text-editor
+    gnome.nautilus
+    gnome.gnome-contacts
+    gnome.gnome-weather
+    gnome.simple-scan
+    gnome-photos
+    evince
+    gnome.gnome-disk-utility    
+    gnome-tour
+  ];
 
 
   # Enable CUPS to print documents.
@@ -75,10 +177,28 @@
     #media-session.enable = true;
   };
 
+  # Bluetooth
+  hardware.bluetooth = {
+    enable = true;
+    powerOnBoot = true;
+  };
+
   # Enable touchpad support (enabled default in most desktopManager).
-  # services.xserver.libinput.enable = true;
+  #services.xserver.libinput.enable = true;
 
   # Define a user account. Don't forget to set a password with ‘passwd’.
+  users.users.junin = {
+    isNormalUser = true;
+    description = "Junin";
+    extraGroups = [
+      "networkmanager"
+      "wheel"
+      "libvirt"
+    ];
+    shell = pkgs.zsh;
+  };
+
+  programs.zsh.enable = true;
 
   nix = {
     settings = {
@@ -96,7 +216,6 @@
 
 
   # Keyboard and Fonts
-
   # Configure keymap in X11
   services.xserver.xkb = {
     layout = "jp";
@@ -114,7 +233,7 @@
 
 
   fonts = {
-    packages= with pkgs; [
+    packages = with pkgs; [
 #      noto-fonts-cjk-serif
 #      noto-fonts-cjk-sans
       noto-fonts-emoji
@@ -175,20 +294,8 @@
     };
   };
 
-  virtualisation.virtualbox.host = {
-    enable = true;
-    enableExtensionPack = true;
-  };
-
   programs.virt-manager = {
     enable = true;
-  };
-
-  
-  # Bluetooth
-  hardware.bluetooth = {
-    enable = true;
-    powerOnBoot = true;
   };
 
 
@@ -204,11 +311,21 @@
     vim # Do not forget to add an editor to edit configuration.nix! The Nano editor is also installed by default.
     wget curl git neofetch p7zip
 #    zed-editor
-    gcc go python3 jdk
+    # Development
+    gcc cmake go python3Full jdk
+    # Audio
     easyeffects
+    # Graphics
+    gimp shotwell
+    # Utilities
     isoimagewriter
+    # Bluetooth
 #    blueman
+    # Podman/Docker
     dive podman-tui docker-compose
+    # Editor
+    kdePackages.kate
+    networkmanagerapplet
   ];
 
   # Some programs need SUID wrappers, can be configured further or are
@@ -219,6 +336,14 @@
   #   enableSSHSupport = true;
   # };
 
+  programs.nano.nanorc = ''
+    set softwrap
+    set linenumbers
+    set autoindent
+    set tabtospaces
+    set tabsize 2
+  '';
+  
   programs.neovim = {
     enable = true;
     configure = {
@@ -263,22 +388,9 @@
   # Enable the OpenSSH daemon.
   # services.openssh.enable = true;
 
-  # Open ports in the firewall.
-  # networking.firewall.allowedTCPPorts = [ ... ];
-  # networking.firewall.allowedUDPPorts = [ ... ];
-  # Or disable the firewall altogether.
-  # networking.firewall.enable = false;
-
-
+  # Flatpak
   services.flatpak.enable = true;
   xdg.portal.enable = true;
-
-  services.tailscale.enable = true;
-  networking.firewall = {
-    enable = true;
-    trustedInterfaces = ["tailscale0"];
-    allowedUDPPorts = [];
-  };
 
 
   # This value determines the NixOS release from which the default
