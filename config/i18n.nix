b3d2bddeee2be74bd9f83b32c750b1c2b588becc
ruad_rofhessa <104673552+magicana-j@users.noreply.github.com>
2024-09-12 11:08:48 +0900

    i18n.nix を更新

--- config/i18n.nix
+++ config/i18n.nix
@@ -38,7 +38,8 @@
             ipafont
             ipaexfont
             source-code-pro
-            source-han-code-jp
+						migmix
+						migu
             nerdfonts
             fira-code
         ];
@@ -48,7 +49,7 @@
             defaultFonts = {
                 serif = ["TakaoPMincho" "Noto Color Emoji"];
                 sansSerif = ["TakaoPGothic" "Noto Color Emoji"];
-                monospace = ["Source Han Code JP" "Noto Color Emoji"];
+                monospace = ["IPAfont" "Noto Color Emoji"];
                 emoji = ["Noto Color Emoji"];
             };
         };
