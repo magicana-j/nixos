3aa68c7ddabe86d2038718d08c990c29040bf3e9
ruad_rofhessa <104673552+magicana-j@users.noreply.github.com>
2024-09-14 08:48:14 +0900

    Create intel.nix

---
+++ config/intel.nix
@@ -0,0 +1,18 @@
+{ config, pkgs, ... }:
+
+{
+
+    hardware.opengl = {
+        enable = true;
+        extraPackages = with pkgs; [
+            onevpl-intel.gpu
+            mesa
+            intel-media-driver
+            intel-media-sdk
+            libvdpau-va-gl
+            vaapiIntel
+            vaapiVdpay
+        ];
+    };
+
+}
