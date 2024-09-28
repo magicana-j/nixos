b3b23f6d870ffd2cce1e435c931de64591236671
ruad_rofhessa <104673552+magicana-j@users.noreply.github.com>
2024-09-28 21:16:23 +0900

    Update gnome.nix

--- config/gnome.nix
+++ config/gnome.nix
@@ -8,30 +8,11 @@
     desktopManager.gnome.enable = true;
   };
 
-  environment.gnome.excludePackages = (with pkgs; [
-    gnome.gnome-terminal
-    gnome.gnome-system-monitor
-    gnome.gnome-screenshot
-    gnome.gnome-music
-    gnome.gnome-keyring
-    gnome.file-roller
-    gnome.eog
-    gnome.yelp
-    gnome.totem
-    gnome.gedit
-    gnome.geary
-    gnome.cheese
-    orca
-    epiphany
-    gnome-text-editor
-    gnome.nautilus
-    gnome.gnome-contacts
-    gnome.gnome-weather
-    gnome.simple-scan
-    gnome-photos
-    evince
-    gnome.gnome-disk-utility
-    gnome-tour
-  ]);
+#  environment.gnome.excludePackages = (with pkgs; [
+#    orca
+#    epiphany
+#    evince
+#    gnome-tour
+#  ]);
 
 }
