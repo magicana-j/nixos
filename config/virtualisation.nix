5bf23904830645ceeade336ca386849047b086a2
ruad_rofhessa <104673552+magicana-j@users.noreply.github.com>
2024-12-02 19:42:22 +0900

    Update virtualisation.nix

--- config/virtualisation.nix
+++ config/virtualisation.nix
@@ -14,24 +14,24 @@
     ];
   };
 
-  virtualisation.libvirtd = {
-    enable = true;
-    qemu = {
-    package = pkgs.qemu_kvm;
-    runAsRoot = true;
-    swtpm.enable = true;
-    ovmf = {
-      enable = true;
-      packages = [(pkgs.OVMF.override {
-      secureBoot = true;
-      tpmSupport = true;
-      }).fd];
-    };
-    };
-  };
-
-  programs.virt-manager = {
-    enable = true;
-  };
+#  virtualisation.libvirtd = {
+#    enable = true;
+#    qemu = {
+#    package = pkgs.qemu_kvm;
+#    runAsRoot = true;
+#    swtpm.enable = true;
+#    ovmf = {
+#      enable = true;
+#      packages = [(pkgs.OVMF.override {
+#      secureBoot = true;
+#      tpmSupport = true;
+#      }).fd];
+#    };
+#    };
+#  };
+#
+#  programs.virt-manager = {
+#    enable = true;
+#  };
 
 }
