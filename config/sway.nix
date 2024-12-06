59ff1d47a9d8e55aa54614387630dc6f4e7d48d5
ruad_rofhessa <104673552+magicana-j@users.noreply.github.com>
2024-12-06 16:15:52 +0900

    Add files via upload

---
+++ config/sway.nix
@@ -0,0 +1,43 @@
+{ config, pkgs, ... }:
+
+{
+  # Sway --
+
+  # Enable the X11 windowing system.
+  # You can disable this if you're only using the Wayland session.
+  services.xserver.enable = true;
+
+  programs.sway = {
+    enable = true;
+    wrapperFeatures.gtk = true;
+    extraPackages = with pkgs; [
+      swaybg swaylock swayidle wl-clipboard wf-recorder
+      mako grim slurp alacritty rofi wofi wlogout
+      networkmanagerapplet
+    ];
+    extraSessionCommands = ''
+      export SDL_VIDEODRIVER=wayland
+      export QT_QPA_PLATFORM=wayland
+      export QT_WAYLAND_DISABLE_WINDOWDECORATION="1"
+      export _JAVA_AWT_WM_NONREPARENTING=1
+      export MOZ_ENABLE_WAYLAND=1
+    '';
+  };
+
+  programs.waybar.enable = true;
+  qt.platformTheme = "qt5ct";
+  hardware.graphics.enable32Bit = true;
+
+  systemd.user.services.kanshi = {
+    description = "kanshi daemon";
+    serviceConfig = {
+      Type = "simple";
+      ExecStart = ''${pkgs.kanshi}/bin/kanshi -c kanshi_config_file'';
+    };
+  };
+
+  security.pam.services.swaylock = {};
+  # -- Sway
+
+}
+
